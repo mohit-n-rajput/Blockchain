1b3ca8e73f0b2f37584da4dc2a094a708829dfca
Mohit N Rajput <41369056+mohit-n-rajput@users.noreply.github.com>
2019-03-12 16:21:48 +0530

    Create modifier.sol

---
+++ modifier.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Purchase {
+    address public seller;
+
+    modifier onlySeller() { // Modifier
+        require(
+            msg.sender == seller,
+            "Only seller can call this."
+        );
+        _;
+    }
+
+    function abort() public view onlySeller { // Modifier usage
+        // ...
+    }
+}
